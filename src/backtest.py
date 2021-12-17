55d405ef3fb0854546eec70333a5df975b0a1e49
Alex Wang <jianuo.wang@duke.edu>
2021-12-17 13:52:57 -0600

    move binance data fetching script to another file

--- src/backtest.py
+++ src/backtest.py
@@ -4,33 +4,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from binance.client import Client
-import datetime
 from simple_mva import SimpleMVA
-
-# YOUR API KEYS HERE
-api_key = ""  # Enter your own API-key here
-api_secret = ""  # Enter your own API-secret here
-
-bclient = Client(api_key=api_key, api_secret=api_secret)
-
-start_date = datetime.datetime.strptime('1 Dec 2021', '%d %b %Y')
-today = datetime.datetime.today()
-
-
-def binanceBarExtractor(symbol):
-    print('working...')
-    filename = '{}_MinuteBars.csv'.format(symbol)
-
-    klines = bclient.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, start_date.strftime(
-        "%d %b %Y %H:%M:%S"), today.strftime("%d %b %Y %H:%M:%S"), 1000)
-    data = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close',
-                        'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
-    data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
-
-    data.set_index('timestamp', inplace=True)
-    data.to_csv(filename)
-    print('finished!')
 
 
 def csv_to_dataframe(filename):
---
+++ src/binance_data.py
@@ -0,0 +1,33 @@
+from binance.client import Client
+import datetime
+import pandas as pd
+
+# YOUR API KEYS HERE
+api_key = ""  # Enter your own API-key here
+api_secret = ""  # Enter your own API-secret here
+
+bclient = Client(api_key=api_key, api_secret=api_secret)
+
+start_date = datetime.datetime.strptime('1 Dec 2021', '%d %b %Y')
+today = datetime.datetime.today()
+
+
+def binanceBarExtractor(symbol, start_date_str):
+    print('working...')
+
+    # start_date_str example: 1 Dec 2021
+    start_date = datetime.datetime.strptime(start_date_str, '%d %b %Y')
+    today = datetime.datetime.today()
+
+    filename = '{}_MinuteBars.csv'.format(symbol)
+
+    klines = bclient.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, start_date.strftime(
+        "%d %b %Y %H:%M:%S"), today.strftime("%d %b %Y %H:%M:%S"), 1000)
+    data = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close',
+                                         'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
+    data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
+
+    data.set_index('timestamp', inplace=True)
+    data.to_csv(filename)
+
+    print('finished!')
