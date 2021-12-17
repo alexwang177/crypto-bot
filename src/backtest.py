cbe52a7d5bcdbe4c1c4e150769fb1fee5e0355ca
Alex Wang <jianuo.wang@duke.edu>
2021-12-17 14:07:18 -0600

    modular plotting code

--- src/backtest.py
+++ src/backtest.py
@@ -11,9 +11,26 @@ def csv_to_dataframe(filename):
     return pd.read_csv(filename, names=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
 
 
+def plot_function(x, y, label):
+    plt.plot(x, y, label=label)
+
+
+def plot_data(x_data, y_data, labels, name):
+    plt.figure(figsize=(24, 12))
+
+    for x, y, label in zip(x_data, y_data, labels):
+        plt.plot(x, y, label=label)
+
+    plt.ylabel("Price (USD)")
+    plt.legend(loc='upper center')
+
+    plt.savefig(name)
+    plt.close()
+
+
 if __name__ == '__main__':
 
-    strat = SimpleMVA(10)
+    strat = SimpleMVA(30)
     df = pd.read_csv('BTCUSDT_MinuteBars.csv')
 
     start_usd = 1000
@@ -52,19 +69,12 @@ if __name__ == '__main__':
         print(f"usd: {usd} btc: {btc} total value (usd): {portfolio_val}")
         print("--------------\n")
 
-    plt.figure(figsize=(24, 12))
-    plt.plot(range(360), btc_price[:360], label="BTC")
-    plt.plot(range(360), mvas[:360], label="Moving Average")
-    plt.ylabel("Price (USD)")
-    plt.legend(loc='upper center')
-    plt.savefig(f'btc_{strat.get_period()}_period_moving_average.png')
+    plot_data(x_data=[range(360), range(360)],
+              y_data=[btc_price[:360], mvas[:360]],
+              labels=['BTC', 'Moving Average'],
+              name=f'btc_{strat.get_period()}_period_moving_average.png')
 
-    plt.close()
-
-    plt.figure(figsize=(24, 12))
-    plt.plot(range(len(portfolio_hold)),
-             portfolio_hold, label="Portfolio Hold")
-    plt.plot(range(len(portfolio)), portfolio, label="Portfolio")
-    plt.ylabel("Price (USD)")
-    plt.legend(loc='upper center')
-    plt.savefig(f'btc_{strat.get_period()}_period_portfolio.png')
+    plot_data(x_data=[range(len(portfolio_hold)), range(len(portfolio))],
+              y_data=[portfolio_hold, portfolio],
+              labels=['Portfolio Hold', 'Portfolio'],
+              name=f'btc_{strat.get_period()}_period_portfolio.png')
