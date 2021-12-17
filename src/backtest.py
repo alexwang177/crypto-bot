f5973a9656f9fe4cd97ea75c9f7514f33d58b92d
Alex Wang <jianuo.wang@duke.edu>
2021-12-17 14:22:34 -0600

    save backtest results in different dir

--- src/backtest.py
+++ src/backtest.py
@@ -4,11 +4,22 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 from simple_mva import SimpleMVA
 
 
 def csv_to_dataframe(filename):
     return pd.read_csv(filename, names=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
+
+
+def save_plot(name):
+    script_dir = os.path.dirname(__file__)
+    results_dir = os.path.join(script_dir, 'backtest_results/')
+
+    if not os.path.isdir(results_dir):
+        os.makedirs(results_dir)
+
+    plt.savefig(os.path.join(results_dir, name))
 
 
 def plot_function(x, y, label):
@@ -21,17 +32,17 @@ def plot_data(x_data, y_data, labels, name):
     for x, y, label in zip(x_data, y_data, labels):
         plt.plot(x, y, label=label)
 
-    plt.ylabel("Price (USD)")
+    plt.ylabel('Price (USD)')
     plt.legend(loc='upper center')
 
-    plt.savefig(name)
+    save_plot(name)
     plt.close()
 
 
 def run_backtest(strat, df):
 
     start_usd = 1000
-    start_btc = start_usd / df["close"][0]
+    start_btc = start_usd / df['close'][0]
 
     usd = start_usd
     btc = None
@@ -42,7 +53,7 @@ def run_backtest(strat, df):
     portfolio_hold = []
 
     for _, row in df.iterrows():
-        price = row["close"]
+        price = row['close']
         action = strat.take_action(float(price))
 
         btc_price.append(price)
@@ -62,9 +73,9 @@ def run_backtest(strat, df):
 
         portfolio_hold.append(start_btc * price)
 
-        print(f"price: {price} action: {action}")
-        print(f"usd: {usd} btc: {btc} total value (usd): {portfolio_val}")
-        print("--------------\n")
+        print(f'price: {price} action: {action}')
+        print(f'usd: {usd} btc: {btc} total value (usd): {portfolio_val}')
+        print('--------------\n')
 
     plot_data(x_data=[range(len(btc_price)), range(len(mvas))],
               y_data=[btc_price, mvas],
