64ad702d02b71215de3f0b296af87c0a688a4cde
Alex Wang <jianuo.wang@duke.edu>
2021-12-17 20:24:56 -0600

    replace order class w action class

--- src/action.py
+++ src/action.py
@@ -1,3 +1,17 @@
-from collections import namedtuple
+class Action:
 
-Action = namedtuple('Action', 'signal currency quantity')
+    def __init__(self, type, currency, quantity, stop_loss=None, take_profit=None):
+        self.type = type
+        self.currency = currency
+        self.quantity = quantity
+        self.stoploss = stop_loss
+        self.take_profit = take_profit
+
+    def get_type(self):
+        return self.type
+
+    def get_currency(self):
+        return self.currency
+
+    def get_quantity(self):
+        return self.quantity
--- src/simple_mva.py
+++ src/simple_mva.py
@@ -2,7 +2,7 @@
     Class that defines strategy for our crypto-trading bot
 '''
 
-from collections import deque, namedtuple
+from collections import deque
 from strategy import Strategy
 from action import Action
 
@@ -35,13 +35,23 @@ class SimpleMVA(Strategy):
 
         # upward trend, price crosses above mva
         if prev_price and prev_price < mva and price > mva:
-            return Action('BUY', port.get_coin(), self.equity_per_trade * port.get_value_in_coin(coin_price=price))
+            return Action('BUY',
+                          port.get_coin(),
+                          self.equity_per_trade *
+                          port.get_value_in_coin(coin_price=price),
+                          stop_loss=0.03,
+                          take_profit=0.06)
 
         # downward trend, price crosses below mva
         if prev_price and prev_price > mva and price < mva:
-            return Action('SELL', port.get_coin(), self.equity_per_trade * port.get_value_in_coin(coin_price=price))
+            return Action('SELL',
+                          port.get_coin(),
+                          self.equity_per_trade *
+                          port.get_value_in_coin(coin_price=price),
+                          stop_loss=0.03,
+                          take_profit=0.06)
 
-        return Action("HOLD", port.get_coin(), None)
+        return Action('HOLD', port.get_coin(), None)
 
     def get_period(self):
         return self.period
